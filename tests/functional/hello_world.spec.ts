07cb73ec9a27847af6c6326158c87395d0319d4c
rizrmdhn <rizrmdhn.work@gmail.com>
2023-12-05 11:07:34 +0800

    Update response in hello_world.spec.ts

--- tests/functional/hello_world.spec.ts
+++ tests/functional/hello_world.spec.ts
@@ -4,5 +4,14 @@ test('display welcome page', async ({ client }) => {
   const response = await client.get('/')
 
   response.assertStatus(200)
-  response.assertBodyContains({ hello: 'world' })
+  response.assertBodyContains({
+    meta: {
+      status: 200,
+      message: 'Welcome to Chat App API',
+    },
+    data: {
+      name: 'Chat App API',
+      version: '1.0.0',
+    },
+  })
 })
